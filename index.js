56718f397b08c8199a9cbe8c5e77853a1f09b6fe
Anne Porter <a.porter9212@gmail.com>
2021-06-10 20:27:44 -0400

    added content to index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+let myBirthYear= 1992;
+let myBirthDay = 12;
+let myBirthMonth = 06;
+console.log(myBirthMonth, myBirthDay, myBirthYear);
+
+const daysInYear = 365;
+console.log(daysInYear);
+
+let name = "Anne";
+let age = "29";
+console.log(name != age)
+
+let donuts = ["glazed", "cake", "filled"];
+let cookies = ["chocolate chip", "sugar", "snickerdoodle", "no bake"];
+console.log(cookies[0])
+
+let athlete = {
+    name                        : "Anne",
+    rank                        : "blue belt",
+    submission                  : "bow and arrow choke"
+
+};
+
+console.log(athlete.rank);
+
+function hello() {
+    console.log("Hello World!");
+}
+ hello();
